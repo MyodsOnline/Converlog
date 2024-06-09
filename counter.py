d9cfadc7adfded25e8866a253067f8d83a42dc29
Andrew Smith <diver.vlz@gmail.com>
2024-06-09 18:15:46 +0700

    Add files via upload

--- counter.py
+++ counter.py
@@ -1,15 +1,17 @@
 from converter import get_dollars_and_rub
 from logfile import log_in_file
-from read_log import read_logfile, extract_number
+
+from read_log import read_logfile, extract_number, count_by_category
+
 from utils import print_separator
 
 
 def counter():
   while True:
     print_separator()
-    option = input('Select: \n1 | e - конвертация бата\n2 | s - записать расходы\n3 | p - вывести отчет\n4 | q - выход\n')
+    option = input('Select:\n1 | e - get exchange rate\n2 | s - записать расходы\n3 | p - вывести отчет\n4 | c - поиск и счет\n5 | q - выход\n')
     
-    if option == '4' or option == 'q':
+    if option == '5' or option == 'q':
       print('bye 🫶')
       print_separator()  
       return False
@@ -18,12 +20,16 @@ def counter():
       get_dollars_and_rub()
     elif option == '2' or option == 's':
       print_separator()
-      log_in_file(input('Расход: '), input('Цель: '))
+      log_in_file(input('Price: '), input('Aim: '))
     elif option == '3' or option == 'p':
       extract_number()
+    elif option == '4' or option == 'c':
+      count_by_category()
     else:
       print(f'странный выбор {option} 🤮')
-
+  
+  
+  #currency = int(input('Select currency: \n1 - if US dollars\n2 - if Thai bath'))
   
   
 if __name__ == '__main__':
--- read_log.py
+++ read_log.py
@@ -3,6 +3,10 @@ from converter import to_rub_converter
 from utils import print_separator
 
 def read_logfile():
+  '''
+  открыть текстовый файл лога
+  вернуть список строк
+  '''
   with open('logfile.txt', 'r', encoding='utf-8') as file:
     log_data = []
     for line in file:
@@ -10,30 +14,56 @@ def read_logfile():
       if text:
         line = text.split(" ")
         log_data.append(line)
-
   return log_data
+
+
+def print_data(data_list):
+  print_separator()
+  data = data_list
+  for item in data:
+    print(' '.join(item))
+
+
+def print_total(data):
+  numbers_list = []
+  print_separator()
+  if data:
+    for el in data:
+      try:
+        cnt = float(el[2])
+        numbers_list.append(cnt)
+      except ValueError:
+        numbers_list.append(0)
+    totat_cost_B = sum(numbers_list)
+    totat_cost_R = to_rub_converter(totat_cost_B)
+    print(f'Total cost - {totat_cost_B} ฿ / {totat_cost_R} ₽')
+  else:
+    print(f'nothing to calculate')
 
 
 def extract_number():
   log_data = read_logfile()
-  numbers_list = []
-  print_separator()
-  for el in log_data:
-    print(' '.join(el))
-    try:
-      cnt = float(el[2])
-      numbers_list.append(cnt)
-    except ValueError:
-      numbers_list.append(0)
-  print_separator()
-  totat_cost_B = sum(numbers_list)
-  totat_cost_R = to_rub_converter(totat_cost_B)
-  print(f'Total cost - {totat_cost_B} ฿ / {totat_cost_R} ₽')
+  print_data(log_data)
+  print_total(log_data)
+  
 
-      
+def count_by_category():
+  print_separator()
+  user_input = input('Искомая строка: ')
+  data = read_logfile()
+  category_data = []
+  for item in data:
+    if item[0] != '_':
+      for element in item:
+        if user_input in element:
+          category_data.append(item)
+  print_data(category_data)
+  print_total(category_data)
+
+        
       
 if __name__ == '__main__':
   #log_data = read_logfile()
-  extract_number()
-  #print(line)
-  #extract_number(line)
+  #extract_number()
+  #print(log_data)
+  count_by_category()
